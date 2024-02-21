edd401c2e58b2faa35b0d7c594a5923f03533c31
Don Evans <jerrydonevans@gmail.com>
2024-02-20 22:03:39 -0800

    feat: created our question, choice, and answer models ran migration

--- BackEnd/SuperQuiz/SuperQuiz/settings.py
+++ BackEnd/SuperQuiz/SuperQuiz/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'super_quiz_app.apps.SuperQuizAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
