c0378b83f3556193acf7ceedf63d1ee58d995e0a
Don Evans <jerrydonevans@gmail.com>
2024-02-21 22:36:24 -0800

    feat: updated question fields to handle multiple choice questions in admin and created quiz model

--- BackEnd/SuperQuiz/super_quiz_app/models.py
+++ BackEnd/SuperQuiz/super_quiz_app/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
+class Quiz(models.Model):
+    title = models.CharField(max_length=100, default="default quiz")
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.title
+
+    def get_questions(self):
+        return self.question_set.all()
 
 class Question(models.Model):
+    # title = models.CharField(max_length = 100)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
