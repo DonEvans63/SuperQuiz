2588ff417550627dbafec2507beca030d8fe00e0
Abigail Olaguivel <abbyog27@gmail.com>
2024-02-22 21:43:45 -0600

    feat: added login function

--- BackEnd/SuperQuiz/super_quiz_app/views.py
+++ BackEnd/SuperQuiz/super_quiz_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 from .models import Question, Choice
 from django.utils import timezone
 from django.views.generic import DetailView
 
+#imports for authentication
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class QuestionDetailView(DetailView):
@@ -23,3 +29,27 @@ class QuestionDetailView(DetailView):
     # return render(request, 'quizes/question_list.html', {'questions': questions})
 def home(request):
     return render(request, "home.html")
+
+def login(request):
+    """ login to app"""
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username=u, password=p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect(f'/user/{u}')
+                else:
+                    print(f'{u} - account has been disabled')
+                    return HttpResponseRedirect('/login')
+            else:
+                print('The username and/or password is incorrect')
+                return HttpResponseRedirect('/login')
+        else:
+            return HttpResponseRedirect('/login')
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', { 'form': form })
