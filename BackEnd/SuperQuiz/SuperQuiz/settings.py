75c69dbdaf4b11fffa0dea610341e8059a7120f4
Abigail Olaguivel <abbyog27@gmail.com>
2024-02-24 18:39:50 -0600

    feat: connected database and created .env

--- BackEnd/SuperQuiz/SuperQuiz/settings.py
+++ BackEnd/SuperQuiz/SuperQuiz/settings.py
@@ -12,6 +12,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+import dj_database_url
+import psycopg2
+
+DATABASE_URL= os.getenv('DATABASE_URL')
+print(DATABASE_URL)
+
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require', port=5432)
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,12 +89,16 @@ WSGI_APPLICATION = 'SuperQuiz.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
+DATABASES['default'] = dj_database_url.config(
+    default = os.getenv(DATABASE_URL),
+    conn_max_age=600, 
+    ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
