4ed40e6f72895ce408e8f29ff283555cf4f4a6cd
Abigail Olaguivel <abbyog27@gmail.com>
2024-02-24 18:45:19 -0600

    feat: merging branches

--- BackEnd/SuperQuiz/super_quiz_app/models.py
+++ BackEnd/SuperQuiz/super_quiz_app/models.py
@@ -11,25 +11,16 @@ class Quiz(models.Model):
         return self.question_set.all()
 
 class Question(models.Model):
-    # title = models.CharField(max_length = 100)
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
-
+    
     def __str__(self):
         return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-
+    is_correct = models.BooleanField(default=False)
+    
     def __str__(self):
         return self.choice_text
-
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.choice_text
-
 
